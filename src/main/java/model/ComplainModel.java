2aac10b8cb7eecc9f6bfa2884faf0de90a59a5ad
Akilach01 <akilapeiris92@gmail.com>
2025-06-20 10:39:07 +0530

    added some methods to complainmodel

--- src/main/java/model/ComplainModel.java
+++ src/main/java/model/ComplainModel.java
@@ -1,24 +1,68 @@
 package model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ComplainModel {
+    
     public void getComplainsByEmail(String email, HttpServletResponse resp, HttpServletRequest req) {
+        ObjectMapper mapper = new ObjectMapper();
+        BasicDataSource dataSource = (BasicDataSource) req.getServletContext().getAttribute("dataSource");
 
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement stmt = connection.prepareStatement("SELECT FROM complains WHERE email = ?");
+            stmt.setString(1, email);
+            ResultSet rst = stmt.executeQuery();
+
+            List<Map<String, Object>> list =new ArrayList<>();
+            while (rst.next()){
+                Map<String, Object> map = new java.util.HashMap<>();
+                map.put("id",rst.getInt("id"));
+                map.put("message",rst.getString("message"));
+                list.add(map);
+            }
+            resp.setContentType("application/json");
+            mapper.writeValue(resp.getWriter(),list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public  deleteComplain( ,  ,  ) {
-    }
 
     public void deleteComplain(int id, HttpServletRequest req, HttpServletResponse resp) {
 
+        ObjectMapper mapper = new ObjectMapper();
+        BasicDataSource dataSource = (BasicDataSource) req.getServletContext().getAttribute("dataSource");
+
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM complains Where id = ? ");
+            statement.setInt(1, id);
+            int rows = statement.executeUpdate();
+
+            resp.setContentType("application/json");
+            mapper.writeValue(resp.getWriter(), Map.of("deleted", rows));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void getAllComplains(HttpServletRequest req, HttpServletResponse resp) {
-        
+
+
+
     }
 
-    public  addNewComplain( ,  ,  ,  ,  ) {
+    public void addNewComplain(String name, String email, String message, HttpServletRequest req, HttpServletResponse resp) {
     }
 }
