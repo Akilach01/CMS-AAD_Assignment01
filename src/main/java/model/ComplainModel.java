a2ea5c34d0bd1ea252e854b0155f1e6b3b9ca41c
Akilach01 <akilapeiris92@gmail.com>
2025-06-20 11:00:56 +0530

    completed complainmodel

--- src/main/java/model/ComplainModel.java
+++ src/main/java/model/ComplainModel.java
@@ -8,13 +8,14 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ComplainModel {
-    
+
     public void getComplainsByEmail(String email, HttpServletResponse resp, HttpServletRequest req) {
         ObjectMapper mapper = new ObjectMapper();
         BasicDataSource dataSource = (BasicDataSource) req.getServletContext().getAttribute("dataSource");
@@ -25,15 +26,15 @@ public class ComplainModel {
             stmt.setString(1, email);
             ResultSet rst = stmt.executeQuery();
 
-            List<Map<String, Object>> list =new ArrayList<>();
-            while (rst.next()){
+            List<Map<String, Object>> list = new ArrayList<>();
+            while (rst.next()) {
                 Map<String, Object> map = new java.util.HashMap<>();
-                map.put("id",rst.getInt("id"));
-                map.put("message",rst.getString("message"));
+                map.put("id", rst.getInt("id"));
+                map.put("message", rst.getString("message"));
                 list.add(map);
             }
             resp.setContentType("application/json");
-            mapper.writeValue(resp.getWriter(),list);
+            mapper.writeValue(resp.getWriter(), list);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -59,10 +60,44 @@ public class ComplainModel {
 
     public void getAllComplains(HttpServletRequest req, HttpServletResponse resp) {
 
+        BasicDataSource dataSource = (BasicDataSource) req.getServletContext().getAttribute("dataSource");
+        try {
+            Connection connection = dataSource.getConnection();
+            ResultSet rst = connection.prepareStatement("SELECT * FROM complains ").executeQuery();
+            List<Map<String, Object>> list = new ArrayList<>();
+            while (rst.next()) {
+                Map<String, Object> map = new java.util.HashMap<>();
+                map.put("id", rst.getInt("id"));
+                map.put("name", rst.getString("name"));
+                map.put("email", rst.getString("email"));
+                map.put("message", rst.getString("message"));
+                list.add(map);
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            resp.setContentType("application/json");
+            mapper.writeValue(resp.getWriter(), list);
 
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void addNewComplain(String name, String email, String message, HttpServletRequest req, HttpServletResponse resp) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        BasicDataSource dataSource = (BasicDataSource) req.getServletContext().getAttribute("dataSource");
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO complains(name, email, message)VALUES (?,?,?)");
+
+            statement.setString(1, name);
+            statement.setString(2, email);
+            statement.setString(3, message);
+            int rows = statement.executeUpdate();
+            resp.setContentType("application/json");
+            mapper.writeValue(resp.getWriter(), Map.of("complaint saved successfully ", rows));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
